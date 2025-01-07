3e517221d2f402d825eec5f15d6f54bc3f242938
Bryan Paredes <bryansparedes2@gmail.com>
2025-01-06 22:45:25 -0600

    Corrigiendo tipados de llamada a API

--- app/lib/get-categories.ts
+++ app/lib/get-categories.ts
@@ -5,7 +5,7 @@ const { STRAPI_HOST } = process.env
 export function getCategories() {
     return Query('product-categories?fields[0]=name&fields[1]=slug&fields[2]=description&populate[image][fields][0]=url')
         .then(res => {
-            return res.data.map(category => {
+            return res.data.map((category: { name: string; slug: string; description: string; image: { url: string } }) => {
                 const { name, slug, description, image: rawImage } = category
                 const image = `${STRAPI_HOST}${rawImage.url}`
 
