a5bbe37bc8f57c19aec9854e82e89a3c8b99f056
Bryan Paredes <bryansparedes2@gmail.com>
2025-01-06 22:40:05 -0600

    Eliminar componentes y secciones innecesarias en layout y page, y reestructurar la página de inicio con nuevos componentes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
-"use client";
-
 import "@/styles/globals.css";
-// import { useState, useEffect } from "react";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
-import { ThemeProvider } from "./components/theme-provder";
 // import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,10 +18,8 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning className="dark">
       <body className={`${inter.className} bg-black text-white`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Navbar />
-          {children}
-        </ThemeProvider>
+        <Navbar />
+        {children}
       </body>
     </html>
   );
