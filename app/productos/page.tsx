c218936df16195dc9d9a980871c06b069ee614b5
Bryan Paredes <bryansparedes2@gmail.com>
2025-01-06 22:51:53 -0600

    Resolviendo problema de tipado en la llamada a cada API

--- app/productos/page.tsx
+++ app/productos/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { getProducts } from "@/lib/get-products";
 import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 
@@ -10,33 +11,40 @@ export default async function ProductsPage() {
       <div className="lg:col-span-3">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {products.length > 0 &&
-            products.map((product) => (
-              <div
-                key={product.id}
-                className="group bg-gray-900 hover:bg-gray-800 p-3 rounded-lg"
-              >
-                <a href={`/products/${product.id}`}>
-                  <div className="relative overflow-hidden rounded-lg bg-gray-100">
-                    <img
-                      src={product.images}
-                      alt={product.name}
-                      height={200}
-                      width={200}
-                      loading="lazy"
-                      decoding="async"
-                      className="w-full h-64 object-cover transition-transform duration-300 group-hover:scale-105"
-                    />
-                  </div>
-                  <div className="mt-4 space-y-2">
-                    <h3 className="text-sm font-medium text-white ">
-                      {product.name}
-                      {product.id}
-                    </h3>
-                    <BlocksRenderer content={product.description} />
-                  </div>
-                </a>
-              </div>
-            ))}
+            products.map(
+              (product: {
+                id: number;
+                name: string;
+                images: string;
+                description: any;
+              }) => (
+                <div
+                  key={product.id}
+                  className="group bg-gray-900 hover:bg-gray-800 p-3 rounded-lg"
+                >
+                  <a href={`/products/${product.id}`}>
+                    <div className="relative overflow-hidden rounded-lg bg-gray-100">
+                      <img
+                        src={product.images}
+                        alt={product.name}
+                        height={200}
+                        width={200}
+                        loading="lazy"
+                        decoding="async"
+                        className="w-full h-64 object-cover transition-transform duration-300 group-hover:scale-105"
+                      />
+                    </div>
+                    <div className="mt-4 space-y-2">
+                      <h3 className="text-sm font-medium text-white ">
+                        {product.name}
+                        {product.id}
+                      </h3>
+                      <BlocksRenderer content={product.description} />
+                    </div>
+                  </a>
+                </div>
+              )
+            )}
         </div>
       </div>
     </div>
--- app/sections/Categories.tsx
+++ app/sections/Categories.tsx
@@ -9,25 +9,26 @@ export default async function Categories() {
     <div className="py-16 px-4 bg-black">
       <div className="max-w-7xl mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          {categories.map(({ name, image }) => (
-            <div
-              key={name}
-              // href={`/products/${slug}`}
-              className="group relative overflow-hidden rounded-lg"
-            >
-              <div className="aspect-[3/4] rounded-lg">
-                <img
-                  src={image}
-                  alt={name}
-                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
-                />
-                <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent" />
-                <div className="absolute bottom-6 left-6">
-                  <h3 className="text-xl font-light">{name}</h3>
+          {categories.map(
+            (categorie: { name: string; slug: string; image: string }) => (
+              <div
+                key={categorie.slug}
+                className="group relative overflow-hidden rounded-lg"
+              >
+                <div className="aspect-[3/4] rounded-lg">
+                  <img
+                    src={categorie.image}
+                    alt={categorie.name}
+                    className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                  />
+                  <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent" />
+                  <div className="absolute bottom-6 left-6">
+                    <h3 className="text-xl font-light">{categorie.name}</h3>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            )
+          )}
         </div>
       </div>
     </div>
