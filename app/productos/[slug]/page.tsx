87f6985c75eb191d5fbe618c79539644a1619e14
Bryan Paredes <bryansparedes2@gmail.com>
2025-01-08 21:45:30 -0600

    agregando tipado y modificando estilos

--- app/productos/[slug]/page.tsx
+++ app/productos/[slug]/page.tsx
@@ -1,14 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { getSingleProduct } from "@/lib/get-products";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import { BlocksRenderer } from "@strapi/blocks-react-renderer";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 
 export default async function ProductPage({
   params,
 }: {
   params: { slug: string };
 }) {
-  const { slug } = params;
+  const { slug } = await params;
   const { product } = await getSingleProduct({ slug });
 
   if (product.length === 0) return null;
@@ -24,14 +30,39 @@ export default async function ProductPage({
       </Link>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-16">{slug}</div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
-        {product.map((singleProduct, index) => (
-          <div key={singleProduct.id ?? index}>
-            {singleProduct.name}
-            {singleProduct.price}
-            <img src={singleProduct.images} alt={singleProduct.name} />
-            <BlocksRenderer content={singleProduct.description} />
-          </div>
-        ))}
+        {product.map(
+          (
+            singleProduct: {
+              id: number;
+              name: string;
+              slug: string;
+              description: any;
+              images: string;
+              price: number;
+              category: { slug: string };
+            },
+            index: number
+          ) => (
+            <div key={singleProduct.id ?? index}>
+              {singleProduct.name}
+              {singleProduct.price}
+              {/* <img src={singleProduct.images} alt={singleProduct.name} /> */}
+              <Carousel opts={{ align: "start" }} className="w-full max-w-lg">
+                <CarouselContent>
+                  <CarouselItem key={index}>
+                    <img
+                      className="rounded-xl w-full"
+                      src={singleProduct.images}
+                      alt={singleProduct.name}
+                    />
+                  </CarouselItem>
+                </CarouselContent>
+              </Carousel>
+              <BlocksRenderer content={singleProduct.description} />
+              <p>{singleProduct.category.slug}</p>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
